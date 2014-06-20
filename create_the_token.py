039debbeabd51f837fa3a2ca059b123d6056d0cc
Shirkey <dev@shirkey.me>
2014-06-20 18:35:03 +0700

    checkpoint

--- create_the_token.py
+++ create_the_token.py
@@ -7,13 +7,13 @@ import macaroons
 # a basic macaroon consists of three elements
 
 # 1) the secret key known only to the credential authority (a web service or software)
-secret = 'kopdar_python_rocks'
+secret = 'kopdar_python_rocks' # // HL
 
 # 2) some interesting metadata about this macaroon (can be anything)
-public = 'kopdar_members_only'
+public = 'kopdar_members_only' # // HL
 
 # 3) a URI/URL, possibly referencing a targeted web service (again, can be anything)
-location = 'http://www.python.or.id/'
+location = 'http://www.python.or.id/' # // HL
 
 # END 1 OMIT
 
@@ -29,24 +29,24 @@ if __name__ == "__main__":
 
     # START 2 OMIT
     # with these three arguments, we can now create the macaroon
-    servis_kopdar = macaroons.create(location, secret, public)
+    servis_kopdar = macaroons.create(location, secret, public) # // HL
 
     # we now hold a reference to our newly instantiated macaroon object
-    print servis_kopdar
+    print(servis_kopdar)
 
     # we can inspect the HMAC signature of this message
-    print '.signature: %s' % servis_kopdar.signature
+    print('.signature: %s' % servis_kopdar.signature) # // HL
 
     # or the other public metadata, like identifier or location
-    print '.identifier: %s' % servis_kopdar.identifier
-    print '.location: %s' % servis_kopdar.location
+    print('.identifier: %s' % servis_kopdar.identifier) # // HL
+    print('.location: %s' % servis_kopdar.location) # // HL
 
-    # or all the metadata + signature as one call
-    print '.inspect(): '
-    print servis_kopdar.inspect()
+    # or all the metadata + signature in a single call
+    print('.inspect():')
+    print servis_kopdar.inspect()  # // HL
 
     # finally, we can convert the macaroon object to a serialized form for transport
-    print '.serialize(): %s' % servis_kopdar.serialize()
+    print '.serialize(): %s' % servis_kopdar.serialize() # // HL
 
     # END 2 OMIT
     get_macaroon()
---
+++ validate_the_token.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from deserialize_the_token import deserialize
+
+macaroon = deserialize()
+
+def validate_verbose(submitted_macaroon=macaroon):
+    # START 1 OMIT
+
+    import macaroons
+
+    # validation occurs using a Verifier object
+    v = macaroons.Verifier() # // HL
+
+    # we'll need the original secret we created earlier
+    secret = 'kopdar_python_rocks'  # // HL
+
+    # we attempt to verify the submitted macaroon
+    print "Is the secret '%s'? -- %s" % (secret, v.verify(submitted_macaroon, secret)) # // HL
+
+    # if we attempt to validate with the wrong secret
+    secret = 'kopdar_python_is_ok-lah' # // HL
+    print "Is the secret '%s'? -- %s" % (secret, v.verify(submitted_macaroon, secret)) # // HL
+
+    # END 1 OMIT
+
+if __name__ == "__main__":
+    validate_verbose()
