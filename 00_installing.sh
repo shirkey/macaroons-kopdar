a1eed3401e5009eacaec92d6287b9a6aeedfde8f
Shirkey <dev@shirkey.me>
2014-06-20 14:17:23 +0700

    checkpoint

--- 00_installing.sh
+++ 00_installing.sh
@@ -4,8 +4,10 @@ if (( $EUID != 0 )); then
         exit
     fi
 
+# START OMIT
 #run as root/sudo
 wget -O - http://ubuntu.hyperdex.org/hyperdex.gpg.key | apt-key add -
 wget -O /etc/apt/sources.list.d/hyperdex.list http://ubuntu.hyperdex.org/trusty.list
 apt-get update
 apt-get install python-macaroons
+# END OMIT
